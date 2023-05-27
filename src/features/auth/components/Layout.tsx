306f740b4117348e2178f15a5bad11d4cfc75944
jack.jarild <jack.jarild@tofindout.se>
2023-05-27 21:50:03 +0200

    Changed login form and layout

--- src/features/auth/components/Layout.tsx
+++ src/features/auth/components/Layout.tsx
@@ -1,5 +1,7 @@
-import { Box } from '@chakra-ui/layout';
+import { Stack, Flex, VStack, Heading, Text, Icon } from '@chakra-ui/react';
 import * as React from 'react';
+import loginBackground5 from '../../../assets/loginBackground5.svg'
+import { ImSearch } from 'react-icons/im';
 
 type LayoutProps = {
     children: React.ReactNode
@@ -8,15 +10,63 @@ type LayoutProps = {
 export const Layout = ({ children }: LayoutProps) => {
     return (
         <>
-            <Box
-                w='100vw'
-                h="100vh"
-                display="flex"
-                justifyContent="center"
-                alignItems="flex-start"
-                as="main">
-                {children}
-            </Box>
+            <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+                <Flex flex={1}
+                    backgroundImage={
+                        `url(${loginBackground5})`
+                    }
+                    backgroundSize={'cover'}
+                    backgroundPosition={'center center'}
+                >
+                    <VStack
+                        w={'full'}
+                        bgGradient={'linear(to-r, blackAlpha.500, blackAlpha.300)'}
+                    >
+                        <Stack maxW={'2xl'} align={'flex-start'}
+                            textAlign={'center'}
+                            spacing={{ base: 3, md: 5 }}
+                            py={{ base: 20, md: 28 }}>
+                            {/* <Heading
+                                color={'black'}
+                                fontWeight={700}
+                                lineHeight={1.2}
+                                fontSize={'7xl'}
+                            >
+                                To
+                                <Text as={'span'} color={'black'} fontWeight={500}>
+                                    Find
+                                </Text>
+                                <Text as={'span'} color={'tfogreen.500'}>
+                                    O
+                                </Text>
+                                <Text as={'span'} color={'black'}>
+                                    ut
+                                </Text>
+                                <Text color={'gray.300'} fontSize={'3xl'} ml={'5'}>
+                                    It's better to know
+                                </Text>
+                            </Heading> */}
+                            <Flex h="20" alignItems="center" mx="8">
+                                <Text fontSize="7xl" fontFamily="monospace" fontWeight="bold">
+                                    TO
+                                </Text>
+                                <Text fontSize="7xl" fontFamily="monospace">
+                                    FIND
+                                </Text>
+                                <Text as={'span'} fontSize="7xl" mt={3} ml={-1}>
+                                    <Icon as={ImSearch} color={'green.400'} transform={'rotateZ(85deg)'} />
+                                </Text>
+                                <Text fontSize="7xl" fontFamily="monospace" fontWeight="bold">
+                                    UT
+                                </Text>
+                            </Flex>
+                        </Stack>
+                    </VStack>
+                </Flex>
+                <Flex p={8} flex={1} align={'center'} justify={'center'}>
+                    {children}
+                </Flex>
+            </Stack>
         </>
     )
 }
--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,11 +1,113 @@
+import { Link } from 'react-router-dom';
+import * as z from 'zod';
 
-type LoginValues = {
-    email: string
-    password: string
-  }
-  
-  type LoginFormProps = {
-    onSuccess: () => void
-  }
+//import { Button } from '@/components/Elements';
+import { Form, InputField, InputGroupField } from '@/components/Form';
+import { Button, Heading, Icon, InputGroup, InputRightElement, Stack } from '@chakra-ui/react';
+import { useLogin } from '@/lib/auth';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
+import { LoginCredentials } from '../api/login';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+//import { useAuth } from '@/lib/auth';
 
-  export const LoginForm = ({ onSuccess }: LoginFormProps) => {}
+const schema = z.object({
+  email: z.string().min(1, 'Required'),
+  password: z.string().min(1, 'Required'),
+});
+
+// type LoginValues = {
+//   email: string;
+//   password: string;
+// };
+
+type LoginFormProps = {
+  onSuccess: () => void;
+};
+
+
+export const LoginForm = ({ onSuccess }: LoginFormProps) => {
+  const login = useLogin();
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <Stack spacing={4} w={'full'} maxW={'md'}>
+      <Heading fontSize={'4xl'} textAlign={'center'}>Welcome back!</Heading>
+      <Form<LoginCredentials, typeof schema>
+        onSubmit={async (values) => {
+          login.mutate(values);
+          onSuccess();
+        }}
+        schema={schema}
+      >
+        {({ register, formState }) => (
+          <>
+            <InputField
+              type="email"
+              label="Email Address"
+              error={formState.errors['email']}
+              registration={register('email')}
+            />
+            {/* <InputGroup> */}
+            {/* <InputField
+              type="password"
+              label="Password"
+              error={formState.errors['password']}
+              registration={register('password')}
+            /> */}
+            <InputGroupField>
+              <InputField
+                type={showPassword ? 'text' : 'password'}
+                error={formState.errors['password']}
+                registration={register('password')}
+              />
+              <InputRightElement h={'full'}>
+                <Button
+                  variant={'ghost'}
+                  onClick={() =>
+                    setShowPassword((showPassword) => !showPassword)
+                  }>
+                  {showPassword ? <Icon as={FiEye} /> : <Icon as={FiEyeOff} />}
+                </Button>
+              </InputRightElement>
+            </InputGroupField>
+            {/* <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }>
+                    {showPassword ? <Icon as={FiEye} /> : <Icon as={FiEyeOff} />}
+                  </Button>
+                </InputRightElement>
+              </InputField>
+            </InputGroup> */}
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'end'}>
+                <Link color={'teal.500'} to={''}>Forgot password?</Link>
+              </Stack>
+              <Button
+                variant={'solid'}
+                isLoading={login.isLoading}
+                type="submit"
+                bgColor={'tfogreen.500'}
+                _hover={{ bgColor: 'tfogreen.300' }}
+              //bgGradient={'linear(to-r, #DB4D9E, #AC42DB, #2D1D8F)'}
+              // _hover={{
+              //   bgGradient: 'linear(to-r, #F4C8E1, #DB4D9E)',
+              // }}
+              >
+                Sign in
+              </Button>
+            </Stack>
+
+          </>
+        )
+        }
+      </Form >
+    </Stack >
+  );
+};

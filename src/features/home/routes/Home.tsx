2dfd32d51baef96ed207fec7430a64a2102b858d
jack.jarild <jack.jarild@tofindout.se>
2023-05-27 21:49:14 +0200

    Fixed animation

--- src/features/home/routes/Home.tsx
+++ src/features/home/routes/Home.tsx
@@ -1,6 +1,6 @@
-import { StatsCard } from "@/components/Elements/StatsCard/StatsCard";
+import { StatsCard } from "@/components/Elements";
 import { SidebarWithHeader } from "@/components/Layout"
-import { Box, Heading, SimpleGrid, chakra } from "@chakra-ui/react"
+import { Box, Heading, SimpleGrid, chakra, useTheme } from "@chakra-ui/react"
 import { motion } from "framer-motion";
 import { useState } from "react";
 
@@ -66,6 +66,33 @@ const InitialTransition = () => {
             },
         },
     };
+    // const blackBox = {
+    //     initial: {
+    //         height: "300px",
+    //         width: "300px",
+    //         marginLeft: 'auto',
+    //         marginRight: 'auto',
+    //         left: 0,
+    //         right: 0,
+    //         textAlign: 'center',
+    //         //bottom: 0,
+    //         borderRadius: '50%',
+    //         opacity: 1,
+    //         //scale: 1
+    //     },
+    //     animate: {
+    //         opacity: 0,
+    //         //scale: 1,
+    //         width: '100%',
+    //         height: '100%',
+    //         borderRadius: 0,
+    //         transition: {
+    //             duration: 3,//1.5,
+    //             //delay: 0.5,
+    //             ease: [0.5, 0.71, 0.2, 1.01]
+    //         },
+    //     },
+    // };
 
     const textContainer = {
         initial: {
@@ -82,7 +109,7 @@ const InitialTransition = () => {
 
     const text = {
         initial: {
-            y: 40,
+            y: 0,
         },
         animate: {
             y: 80,
@@ -98,40 +125,40 @@ const InitialTransition = () => {
     console.log('InitialTransition called')
 
     return (
-        // <Box pos={'absolute'} zIndex={50} w={'100%'} bgColor={"green.600"} inset={inset} display={'flex'} alignItems={'center'} justifyContent={'center'}>
-            <motion.div
-                style={{ position: 'absolute', zIndex: 50, width: '100%', backgroundColor: 'green', inset: inset, display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-                initial="initial"
-                animate="animate"
-                variants={blackBox}
-                onAnimationStart={() => setInset(0)}
-                onAnimationComplete={() => setInset('unset')}
-            >
-                <motion.svg variants={textContainer} style={{ position: 'absolute', display: 'flex' }} z={50}>
-                    <pattern
-                        id="pattern"
-                        patternUnits="userSpaceOnUse"
-                        width={750}
-                        height={800}
-                        color="white"
-                    >
-                        <rect style={{ width: '100%', height: '100%', fill: 'currentcolor' }} />
-                        <motion.rect
-                            variants={text}
-                            style={{ width: '100%', height: '100%', color: 'pink', fill: 'currentcolor' }}
-                        />
-                    </pattern>
-                    <text
-                        style={{ fontSize: '3rem', fontWeight: 'bold', fill: "url(#pattern)" }}
-                        textAnchor="middle"
-                        x="50%"
-                        y="50%"
-                    >
-                        ToFindOut
-                    </text>
-                </motion.svg>
-            </motion.div>
-        // </Box>
+        <Box pos={'absolute'} zIndex={50} w={'100%'} bgColor={"tfogreen.500"} inset={inset} display={'flex'} alignItems={'center'} justifyContent={'center'}
+            as={motion.div}
+            initial="initial"
+            animate="animate"
+            variants={blackBox}
+            onAnimationStart={() => setInset(0)}
+            onAnimationComplete={() => setInset('unset')}>
+            <motion.svg variants={textContainer} style={{ position: 'absolute', display: 'flex', width: '100%' }} z={50}>
+                <pattern
+                    id="pattern"
+                    patternUnits="userSpaceOnUse"
+                    width={750}
+                    height={800}
+                    color="white"
+                >
+                    <rect style={{ width: '100%', height: '100%', fill: 'currentcolor' }} />
+                    <Box as={motion.rect}
+                        variants={text}
+                        color={'tfopink.500'}
+                        w={'100%'}
+                        h={'100%'}
+                        fill={'currentcolor'}
+                    />
+                </pattern>
+                <text
+                    style={{ fontSize: '6rem', fontWeight: 'bold', fill: "url(#pattern)" }}
+                    textAnchor="middle"
+                    x="50%"
+                    y="50%"
+                >
+                    ToFindOut
+                </text>
+            </motion.svg>
+        </Box>
     );
 };
 

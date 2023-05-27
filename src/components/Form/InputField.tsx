6fdbe912b67ab0b5316f52409cc296e2060a6e8c
jack.jarild <jack.jarild@tofindout.se>
2023-05-27 23:02:44 +0200

    Added more props to InputFieldProps

--- src/components/Form/InputField.tsx
+++ src/components/Form/InputField.tsx
@@ -4,18 +4,26 @@ import { FieldWrapper, FieldWrapperPassThroughProps } from './FieldWrapper';
 import { Input } from '@chakra-ui/react';
 
 type InputFieldProps = FieldWrapperPassThroughProps & {
-  type?: 'text' | 'email' | 'password';
-  className?: string;
-  registration: Partial<UseFormRegisterReturn>;
+  type?: 'text' | 'email' | 'password'
+  className?: string
+  registration: Partial<UseFormRegisterReturn>
+  placeHolder?: string
+  size?: 'xs' | 'sm' | 'md' | 'lg'
+  variant?: 'outline' | 'filled' | 'flushed' | 'unstyled' 
 };
 
+export type InputFieldPassThroughProps = Omit<InputFieldProps, 'className' | 'children'>;
+
 export const InputField = (props: InputFieldProps) => {
-  const { type = 'text', label, className, registration, error } = props;
+  const { type = 'text', label, className, registration, placeHolder, size = 'md', variant = 'outline', error } = props;
   return (
     <FieldWrapper label={label} error={error}>
       <Input
         type={type}
         className={className}
+        placeholder={placeHolder}
+        size={size}
+        variant={variant}
         {...registration}
       />
     </FieldWrapper>

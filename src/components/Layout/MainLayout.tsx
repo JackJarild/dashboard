a0f36689400e86803f4e9572630574bf963831e6
JackJarild <jaaackk@hotmail.com>
2023-05-31 22:13:43 +0200

    Added outlet to MainLayout

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,24 +1,34 @@
-import { useState } from "react";
 import { SidebarWithHeader } from "./SidebarWithHeader";
+import { Suspense } from "react";
+import { Box, CircularProgress } from "@chakra-ui/react";
+import { Navigate, useOutlet } from "react-router-dom";
+import { useUser } from "@/lib/auth";
 
+export const MainLayout = () => {
+    const outlet = useOutlet()
+    const user = useUser()
 
-type MainLayoutProps = {
-    children: React.ReactNode;
-};
-
-export const MainLayout = ({ children }: MainLayoutProps) => {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
+    if(!user.data) {
+        return <Navigate to='/' />
+    }
 
     return (
-        <>
+        <Suspense
+            fallback={
+                <Box
+                    w='100vw'
+                    h="100vh"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <CircularProgress isIndeterminate color='green.300' />
+                </Box>
+            }
+        >
             <SidebarWithHeader>
-                {children}
+                {outlet}
             </SidebarWithHeader>
-            {/* <Container component={'main'} maxWidth={'xl'} sx={{ mt: 4 }}>
-                <Grid container minHeight={'100vh'}>
-                    {children}
-                </Grid>
-            </Container> */}
-        </>
-    );
-};
+        </Suspense>
+    )
+}

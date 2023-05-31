6cf008ffa9516cbf13d18867420e82e7e43fa8e2
JackJarild <jaaackk@hotmail.com>
2023-05-31 22:09:39 +0200

    Removed AuthUser type

--- src/features/auth/types/index.ts
+++ src/features/auth/types/index.ts
@@ -1,20 +1,11 @@
-export type AuthUser = {
-    id: number;
-    email: string;
-    firstName: string;
-    lastName: string;
-    bio: string;
-    role: 'ADMIN' | 'USER';
-  };
-
 export type UserResponse = {
-    id: number;
-    firstName: string;
-    lastName: string,
-    email: string;
-    companyId: number;
-    phoneNumber: string;
-    language: string;
-    isActive: boolean;
-    token: string;
+    id: number
+    firstName: string
+    lastName: string
+    email: string
+    companyId: number
+    phoneNumber: string
+    language: string
+    isActive: boolean
+    token: string
 }

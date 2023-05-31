cf8eaf807dbc8ac5e36b6e81b3f250297d898fa7
JackJarild <jaaackk@hotmail.com>
2023-05-31 22:03:35 +0200

    Added functions to store user

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,7 +1,13 @@
+import { UserResponse } from "@/features/auth";
+
 const storagePrefix = 'dashboard_';
 
 export const storage = {
   getToken: () => JSON.parse(window.localStorage.getItem(`${storagePrefix}token`) as string),
   setToken: (token: string) => window.localStorage.setItem(`${storagePrefix}token`, JSON.stringify(token)),
-  clearToken: () => window.localStorage.removeItem(`${storagePrefix}token`)
+  clearToken: () => window.localStorage.removeItem(`${storagePrefix}token`),
+  // Remove?
+  getUser: (): UserResponse => JSON.parse(window.localStorage.getItem(`${storagePrefix}user`) as string),
+  setUser: (user: UserResponse) => window.localStorage.setItem(`${storagePrefix}user`, JSON.stringify(user)),
+  clearUser: () => window.localStorage.removeItem(`${storagePrefix}user`),
 };

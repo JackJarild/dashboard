94356f942b2c6e8fd5fac5f493a01fff5687d324
JackJarild <jaaackk@hotmail.com>
2023-06-04 14:33:12 +0200

    Added Archive route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ const { ErrorPage } = lazyImport(() => import('@/features/home'), 'ErrorPage');
 const { NotFound } = lazyImport(() => import('@/features/home'), 'NotFound');
 const { Settings } = lazyImport(() => import('@/features/settings'), 'Settings');
 const { Order } = lazyImport(() => import('@/features/order'), 'Order');
+const { Archive } = lazyImport(() => import('@/features/archive'), 'Archive');
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +27,7 @@ export const router = createBrowserRouter(
           <Route path="/" element={<Home isFirstMount={true} />} />
           <Route path="order" element={<Order />} />
           <Route path="settings" element={<Settings />} />
+          <Route path="archive" element={<Archive />} />
           <Route path='*' element={<NotFound />} />
         </Route>
       </Route>
